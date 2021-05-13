3a185b946a04a337be2df343d581b62ba99dd3c8
Shinsuke  May <s.seanmay@gmail.com>
2021-05-13 18:18:40 +0900

    add semicolons

--- src/02-oldest-package-name/index.js
+++ src/02-oldest-package-name/index.js
@@ -2,7 +2,7 @@ import {apiGetter} from '../api'
  
 module.exports = async function oldestPackageName() {
   try { 
-    const res = await apiGetter()
+    const res = await apiGetter();
     
     // For every item in the content array, convert 'date' string to Date object, 
     // then compare the current value to acucmulator and check which is older. Return oldest.
--- src/03-organise-maintainers/index.js
+++ src/03-organise-maintainers/index.js
@@ -10,33 +10,33 @@ function makeAlphabetical(arr) {
   for (let item of sortedUsername) {
     item.packageNames.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase())) 
   }
-  return sortedUsername
+  return sortedUsername;
 }
 
 // Helper function that takes an array of objects and identifies maintainers
 // of each package. Add mainainer's username with name of package if maintainer 
 // is not in the 'mapped' list. If username exists, add name of package to package array.
 function outer(arr) {
-  let mapped = []
+  let mapped = [];
   for (let item of arr) {
-    let pkg = item.package.name
+    let pkg = item.package.name;
     for (let mtainer of item.package.maintainers) {
-      let username = mtainer.username
+      let username = mtainer.username;
       let known = mapped.findIndex(c => c.username == username);
       if (known == -1) {
         mapped.push({username, packageNames: [pkg]});
       } else {
-        mapped[known].packageNames.push(pkg)
+        mapped[known].packageNames.push(pkg);
       }
     }
   }
-  return makeAlphabetical(mapped)
+  return makeAlphabetical(mapped);
 }
 
 module.exports = async function organiseMaintainers() {
   try { 
-    const res = await apiGetter()
-    return outer(res.data.content)
+    const res = await apiGetter();
+    return outer(res.data.content);
   } catch (err) {
     console.log(err);
   }
